0e0fc39da8f91d080a873aee299b6b62e43a6dd1
otoiron <potbelly.dansyaku@gmail.com>
2025-08-26 17:31:46 +0900

    不要なファイルの削除

--- src/app/api/tasks/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { Task } from "@/types/task";
-import tasks from "@/data/tasks.json";
-
-export async function GET() {
-  return NextResponse.json(tasks as Task[]);
-}
